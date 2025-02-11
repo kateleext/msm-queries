5ba4f264fd68774a72932be04898758aed22b4b9
kateleext <83829378+kateleext@users.noreply.github.com>
2025-02-11 06:35:51 +0000

    Done

--- app/controllers/actor_controller.rb
+++ app/controllers/actor_controller.rb
@@ -1,15 +1,13 @@
 class ActorController < ApplicationController
   def index
-    @list_of_actors = Actor.all.order({:dob => :asc})
+    @list_of_actors = Actor.all
     render(:template => "index/actors")
   end
 
   def details
     actor_id = params.fetch("id")
     @actor = Actor.all.where(:id => actor_id).first
-    movie_ids = []
-    Character.all.where(:actor_id => actor_id).each do |character| movie_ids.push(character.movie_id) end
-    @filmography = Movie.all.where(:id => movie_ids)
+    @filmography = Character.all.where(:actor_id => actor_id)
     render(:template => "details/actor_details")
   end
 end
